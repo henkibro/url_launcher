4045d8293c8d15e29c12ed027f4d739f38ac1df8
Sarah Zakarias <zarah@google.com>
2017-05-17 23:05:30 +0200

    Unify plugin names to use underscores (#63)

---
+++ example/ios/Runner/AppDelegate.m
@@ -0,0 +1,13 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  return YES;
+}
+
+@end
